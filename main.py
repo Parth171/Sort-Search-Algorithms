13930f1212bbf15b5b6beb8a3543311d0bb4a499
p.sakpal <p.sakpal@share.epsb.ca>
2023-10-16 13:48:58 -0600

    Finished Iterative selection sort on the data

--- main.py
+++ main.py
@@ -7,10 +7,57 @@ date-created: 10/12/2023
 
 # import libraries
 import csv
+import pathlib
+import sqlite3
+
+
+
+DATABASE_FILE = "supehero.db"
+
+FIRST_RUN = True
+
+# Checks to see if database file already exists
+if (pathlib.Path.cwd() / DATABASE_FILE).exists():
+    FIRST_RUN = False
+
+CONNECTION = sqlite3.connect(DATABASE_FILE)
+CURSOR = CONNECTION.cursor()
+
+
+
+# Sort the Data using Selection Sort
+
+def selectionSort(LIST):
+    '''
+    Selection sorts the data by the year column
+    :param LIST: list
+    :return: List (sorted)
+    '''
+
+    for i in range(len(LIST)-1):
+        MIN_INDEX = i
+
+        for j in range(i+1, len(LIST)):
+            if LIST[j][9] < LIST[MIN_INDEX][9]:
+                MIN_INDEX = j
+
+
+        if LIST[MIN_INDEX] < LIST[i]:
+            TEMP = LIST[i]
+            LIST[i] = LIST[MIN_INDEX]
+            LIST[MIN_INDEX] = TEMP
+
+
+
+
+
+
+
+
+
 
 
 # Extract data from the csv
-
 def getData(fileName):
     '''
     gets the raw data from the Superhero File
@@ -28,6 +75,11 @@ def getData(fileName):
         DATA.append(line)
 
     TITLE = DATA.pop(0)
+
+
+    for i in range(len(DATA)):
+        if DATA[i][9].isnumeric():
+            DATA[i][9] = int(DATA[i][9])
 
     return DATA
 
@@ -61,10 +113,49 @@ def userInput(DATA):
 
 
 
-if __name__ == "__main__":
-    DATA = getData("comicBookCharData_mixed.csv")
 
-    userInput(DATA)
+def createDatabase(LIST):
+    '''
+    Create the superhero datbase
+    :param LIST: list
+    :return: None
+    '''
+
+    global CONNECTION, CURSOR
+
+    CURSOR.execute("""
+        CREATE TABLE
+            superhero (
+                superhero_id,
+                name,
+                ID,
+                align,
+                eye,
+                hair,
+                alive,
+                appearance,
+                year,
+                brand
+                
+            )
+    ;""")
+
+
+
+if __name__ == "__main__":
+
+    DATA = getData("comicBookCharData_mixed.csv")
+    print(int(DATA[0][9]))
+    print(DATA[0][9])
+
+    '''print(DATA)
+    selectionSort(DATA)
+    print("######################################################################")
+    print(DATA)'''
+
+
+
+
 
 
 
