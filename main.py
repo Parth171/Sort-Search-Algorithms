ccdb1630492e13ad0d09093337f50ba3599169f1
p.sakpal <p.sakpal@share.epsb.ca>
2023-10-25 13:44:56 -0600

    Added Finishing Touches and Internal Comments. Finished the recursive project.

--- main.py
+++ main.py
@@ -9,12 +9,14 @@ date-created: 10/12/2023
 import csv
 import pathlib
 import sqlite3
-from tabulate import tabulate
+from tabulate import tabulate # Used to display the Superhero Information neatly to the User
 
 
-
+# Creates the Database File
 DATABASE_FILE = "superhero.db"
 
+
+# Checks if this is the first time running the program
 FIRST_RUN = True
 
 # Checks to see if database file already exists
@@ -24,13 +26,14 @@ if (pathlib.Path.cwd() / DATABASE_FILE).exists():
 CONNECTION = sqlite3.connect(DATABASE_FILE)
 CURSOR = CONNECTION.cursor()
 
+# Two Lists used to separate the DC and Marvel Data to individually sort each by the Superhero ID
 MARVEL_DATA = []
 DC_DATA = []
 
 
 def menu():
     """
-    presents a menu to the user for them to choose an action
+    Explains the program and presents a menu to the user for them to pick an action
     :return: int
     """
 
@@ -39,16 +42,21 @@ def menu():
     
     Welcome to the Recursive SUPER HERO SEARCH AND SORT!
     
-    This program will allow you to search for a superhero by using their SuperHero ID, View the sorted SuperHero 
-    database, and even add your own superhero to the Database! (Note: Please enter the ID accurately and exactly) 
+    This program will allow you to search for a Superhero by using their SuperHero ID, view the sorted Superhero 
+    database, and even add your own superhero to the Database! (Note: Please enter the ID accurately and exactly as it is meant to appear) 
+    
     
     1. Search for SuperHero using their Superhero ID
-    2. View Sorted Superhero Database (Sorted by their SuperHero ID in ascending order with DC Superheros appearing first,followed by Marvel SuperHeros.) 
+    
+    2. View Sorted Superhero Database 
+    
     3. Add your own Superhero
+    
     4. Exit the program
     ___________________________________________________________________________________________________________________
     """)
 
+    # Error checks the input to see if it is a numeric value
     USER_INPUT = input("Select your choice: ")
     if USER_INPUT.isnumeric() is False:
         print("Enter a number value.")
@@ -63,14 +71,86 @@ def menu():
         print("Select an option from the menu")
         return menu()
 
-# Sort the Data using Selection Sort
+
+def createDatabase():
+    '''
+    Create the superhero datbase
+    :return: None
+    '''
+
+    global CONNECTION, CURSOR
+
+    CURSOR.execute("""
+        CREATE TABLE
+            superhero (
+                superhero_id,
+                name,
+                ID,
+                align,
+                eye,
+                hair,
+                alive,
+                appearance,
+                first_appearance,
+                year,
+                brand
+
+            )
+    ;""")
 
 
+# Extract data from the csv
+def getRawData(fileName):
+    '''
+    Gets the raw data from the Superhero CSV File
+    :param fileName: CSV file
+    :return: 2D Array
+    '''
+
+
+    DATA = []
+    FILE = open(fileName)
+
+    LINES = csv.reader(FILE)
+
+    for line in LINES:
+        DATA.append(line)
+
+    TITLE = DATA.pop(0)
+
+
+    for i in range(len(DATA)):
+        if DATA[i][9].isnumeric():
+            DATA[i][9] = int(DATA[i][9])
+
+    return DATA, TITLE
+
+
+def addData(LIST):
+    """
+    Adds the sorted Superhero list to the database
+    :param LIST: List (Sorted by Superhero ID)
+    :return: None
+    """
+
+    global CONNECTION, CURSOR
+
+    for i in range(len(LIST)):
+        CURSOR.execute("""
+            INSERT INTO
+                superhero
+            VALUES (
+                ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?
+            )
+
+        ;""", LIST[i])
+
+        CONNECTION.commit()
 
 def quickSort(LIST, FIRST_INDEX, LAST_INDEX):
     """
     Quick Sorts the data (recursively)
-    :param LIST: List
+    :param LIST: List (Raw Data)
     :param FIRST_INDEX: int
     :param LAST_INDEX: int
     :return: List (sorted)
@@ -111,107 +191,30 @@ def quickSort(LIST, FIRST_INDEX, LAST_INDEX):
     return LIST
 
 
-# Extract data from the csv
-def getRawData(fileName):
-    '''
-    gets the raw data from the Superhero File
-    :param fileName: CSV file
-    :return: 2D Array
-    '''
-
-
-    DATA = []
-    FILE = open(fileName)
-
-    LINES = csv.reader(FILE)
-
-    for line in LINES:
-        DATA.append(line)
-
-    TITLE = DATA.pop(0)
-
-
-    for i in range(len(DATA)):
-        if DATA[i][9].isnumeric():
-            DATA[i][9] = int(DATA[i][9])
-
-    return DATA, TITLE
-
-
 def userInput(DATA):
     '''
-    gets the user input for Superhero ID and error checks
+    Gets the user input for Superhero ID and error checks
     :param DATA: 2D array
-    :return: String
+    :return: Str (Superhero ID)
     '''
 
 
-    INPUT = input(str("What is the superhero ID: "))
+    INPUT = input(str("What is the Superhero ID: "))
 
     ID = []
 
-
+    # Error checks to see if the inputted Superhero ID is in the database
     for i in range(len(DATA)):
             ID.append(DATA[i][0])
 
 
     if INPUT not in ID:
-        print("NOT VALID INPUT!")
+        print("INVALID ENTRY")
         return userInput(DATA)
 
     else:
         return INPUT
 
-
-def createDatabase():
-    '''
-    Create the superhero datbase
-    :return: None
-    '''
-
-    global CONNECTION, CURSOR
-
-    CURSOR.execute("""
-        CREATE TABLE
-            superhero (
-                superhero_id,
-                name,
-                ID,
-                align,
-                eye,
-                hair,
-                alive,
-                appearance,
-                first_appearance,
-                year,
-                brand
-                
-            )
-    ;""")
-
-
-def addData(LIST):
-    """
-    Adds the sorted list to the database
-    :param LIST: sorted list of data
-    :return: None
-    """
-
-    global CONNECTION, CURSOR
-
-
-    for i in range(len(LIST)):
-
-        CURSOR.execute("""
-            INSERT INTO
-                superhero
-            VALUES (
-                ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?
-            )
-        
-        ;""", LIST[i])
-
-        CONNECTION.commit()
 
 
 def binarySearch(LIST, VALUE):
@@ -224,7 +227,7 @@ def binarySearch(LIST, VALUE):
 
     MIDPOINT = len(LIST) // 2
 
-    # base case
+    # Base case
     if LIST[MIDPOINT][0][1:] == VALUE[1:]:
         return MIDPOINT, LIST
     else:
@@ -239,7 +242,7 @@ def binarySearch(LIST, VALUE):
 
 def getData():
     """
-    gets the data from the database (this is to ensure that if the user has added a new member, the member can be displayed)
+    Get the data from the database (this is to ensure that if the user has added a new member, the member can be displayed when searching)
     :return: list - from the database
     """
 
@@ -282,7 +285,7 @@ def addSuperHero():
     USER_DATA.append(input("YEAR: "))
     USER_DATA.append(input("Brand (Marvel or DC): "))
 
-
+    # Checks to see is all the required data has been inputted
     if USER_DATA[0] == "" or USER_DATA[1] == "" or USER_DATA[10] == "":
         print("You have not entered all the required data")
         return addSuperHero()
@@ -307,14 +310,13 @@ if __name__ == "__main__":
     if FIRST_RUN:
         createDatabase()
 
-
-
         for i in range(len(RAW_DATA)):
             if RAW_DATA[i][0][0] == "M":
                 MARVEL_DATA.append(RAW_DATA[i])
             else:
                 DC_DATA.append(RAW_DATA[i])
 
+        # Quicksorts the DC and Marvel Data separately and combines it into one List
         SORTED_DATA = quickSort(DC_DATA, 0, len(DC_DATA)-1) + quickSort(MARVEL_DATA, 0, len(MARVEL_DATA)-1)
 
 
@@ -322,6 +324,7 @@ if __name__ == "__main__":
 
     while True:
 
+        # Fetches the data from the database
         DATA = getData()
 
         for i in range(len(DATA)):
@@ -330,32 +333,39 @@ if __name__ == "__main__":
                 if DATA[i][j] == "":
                     DATA[i][j] = "N/A"
 
+
+        # INPUTS #
         CHOICE = menu()
 
+
+        # PROCESSING #
         if CHOICE == 1:
+
 
             USER_SEARCH = userInput(DATA)
             VALUE_INDEX, USER_LIST = binarySearch(DATA, USER_SEARCH)
 
-
-
+            # OUTPUTS #
             print(f"\n{tabulate(USER_LIST[VALUE_INDEX:VALUE_INDEX+1], headers=TITLES)}")
 
 
-
+        # INPUTS #
         if CHOICE == 2:
 
+            # PROCESSING AND OUTPUTS
             print(tabulate(DATA, headers=TITLES)) # prints
 
 
-
+        # INPUTS #
         if CHOICE == 3:
+            # PROCESSING #
             addSuperHero()
+            # OUTPUTS #
             print("Successfully added your superhero!")
 
-
-
+        # INPUTS #
         if CHOICE == 4:
+            # PROCESSING AND OUTPUTS
             print("Thanks for using the program!")
             break
 
